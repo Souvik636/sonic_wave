0f60940806b5bb2df62071786ca3e568e7a12375
souvik636 <souviksaha636@gmail.com>
2026-08-21 11:33:53 +0530

    chore: bump version to 1.2.11

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -2,6 +2,6 @@
 class AppVersion {
   AppVersion._();
 
-  /// Current application version (matches release tag e.g. "1.2.10").
-  static const String current = '1.2.10';
+  /// Current application version (matches release tag e.g. "1.2.11").
+  static const String current = '1.2.11';
 }
